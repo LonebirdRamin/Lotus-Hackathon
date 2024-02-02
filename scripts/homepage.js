64d3be2c971b334b9e8115d81a7440f253625b80
LonebirdRamin <dsl0nebird@gmail.com>
2024-02-02 22:54:17 +0700

    Finish ingredient.html
    - Ingredient is done
    - Able to transfers data between page

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,12 +1,44 @@
-function setCookie(name, days){
-    let expires = "";
-    if (days){
-        const date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
+const ingredients = [
+    { name: "Chicken Breast", image: "./source/image/chicken-breast.webp" },
+    { name: "Tenderloin", image: "./source/image/tenderlion.png" },
+    { name: "Egg", image: "./source/image/egg.png" },
+    { name: "Butter", image: "./source/image/7301072.webp" },
+    { name: "Holy Basil", image: "./source/image/holybasil.png" },
+    { name: "Shitake", image: "./source/image/shitake.png" },
+    { name: "Minced Pork", image: "./source/image/minced-pork.webp" },
+    { name: "Lime", image: "./source/image/lime.png" },
+    { name: "Apple", image: "./source/image/apple.png" },
+    { name: "Rice", image: "./source/image/rice.png" }
+];
+
+function displayCheckedIngredients() {
+    const container = document.querySelector(".basket-content");
+    const savedIngredients = localStorage.getItem('checkedIngredients');
+    if (savedIngredients) {
+        const checkedIngredients = JSON.parse(savedIngredients);
+        checkedIngredients.forEach(ingredientName => {
+            const ingredient = ingredients.find(item => item.name === ingredientName);
+            if (ingredient) {
+                const div = document.createElement("div");
+                div.classList.add("checked-ingredient");
+
+                const img = document.createElement("img");
+                img.id = "ingredient-image";
+                img.src = ingredient.image;
+                img.alt = ingredient.name;
+
+                const p = document.createElement("p");
+                p.textContent = ingredient.name;
+
+                div.appendChild(p);
+                div.appendChild(img);
+                container.appendChild(div);
+            }
+        });
     }
-    document.cookie = name + "=" + expires + "; path=/";
 }
+
+window.addEventListener('load', displayCheckedIngredients);
 
 function naviagateToRecipePage(){
     window.location.href = "foodrecipe.html";
--- scripts/ingredient.js
+++ scripts/ingredient.js
@@ -1,13 +1,13 @@
 const ingredients = [
-    { name: "Chicken Breast", image: "./source/image/chicken-breast.png" },
-    { name: "Tenderlion", image: "./source/image/tenderlion.png" },
+    { name: "Chicken Breast", image: "./source/image/chicken-breast.webp" },
+    { name: "Tenderloin", image: "./source/image/tenderlion.png" },
     { name: "Egg", image: "./source/image/egg.png" },
     { name: "Butter", image: "./source/image/7301072.webp" },
     { name: "Holy Basil", image: "./source/image/holybasil.png" },
-    { name: "Shitake", image: "./source/image/shitake.jfif" },
+    { name: "Shitake", image: "./source/image/shitake.png" },
     { name: "Minced Pork", image: "./source/image/minced-pork.webp" },
     { name: "Lime", image: "./source/image/lime.png" },
-    { name: "Apple", image: "./source/image/apple.jfif" },
+    { name: "Apple", image: "./source/image/apple.png" },
     { name: "Rice", image: "./source/image/rice.png" }
 ];
 
@@ -23,9 +23,19 @@ function createIngredientItem(ingredient) {
     const p = document.createElement("p");
     p.textContent = ingredient.name;
 
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("invisible-checkbox");
+
     div.appendChild(img);
     div.appendChild(p);
+    div.appendChild(checkbox)
 
+    div.onclick = function() {
+        checkbox.checked = !checkbox.checked;
+        div.classList.toggle("checked", checkbox.checked);
+        sendData();
+    };
     return div;
 }
 
@@ -35,6 +45,36 @@ function addIngredients() {
         const ingredientItem = createIngredientItem(ingredient);
         container.appendChild(ingredientItem);
     });
+    restoreCheckedIngredients();
+}
+
+function sendData() {
+    const checkedIngredients = [];
+    const checkboxes = document.querySelectorAll('.checked .invisible-checkbox');
+    checkboxes.forEach(checkbox => {
+        const ingredientName = checkbox.parentNode.querySelector('p').textContent;
+        console.log(ingredientName)
+        checkedIngredients.push(ingredientName);
+    });
+    localStorage.setItem('checkedIngredients', JSON.stringify(checkedIngredients));
+}
+
+function restoreCheckedIngredients() {
+    const savedIngredients = localStorage.getItem('checkedIngredients');
+    if (savedIngredients) {
+        const checkedIngredients = JSON.parse(savedIngredients);
+        const checkboxes = document.querySelectorAll('.invisible-checkbox');
+        checkboxes.forEach(checkbox => {
+            const nextElement = checkbox.parentNode.querySelector('p').textContent;
+            console.log(nextElement)
+            if (checkedIngredients.includes(nextElement)) { 
+                checkbox.checked = true;
+                checkbox.parentElement.classList.add("checked");
+            }
+        });
+    }
 }
 
 window.addEventListener("load", addIngredients);
+document.addEventListener("click", toggleCheck);
+document.querySelectorAll('.ingredient-item').forEach(item => item.addEventListener('click', toggleCheck));
