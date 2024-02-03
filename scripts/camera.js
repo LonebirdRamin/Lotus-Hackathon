f04927f141e21274a67618e28dd7831372be241f
Ramin <dsl0nebird@gmail.com>
2024-02-03 13:27:06 +0700

    Camera fixing temp

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,27 +1,18 @@
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const snapButton = document.getElementById('snap');
+(function() {
+    var video = document.getElementById('video');
+    var snapButton = document.getElementById('snap');
 
-const constraints = {
-  video: true
-};
+    if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+        const promise = navigator.mediaDevices.getUserMedia({video: true})
+        promise.then(function(stream) {
+            video.srcObject = stream;
+            video.play();
+        }).catch(function(err) {
+            console.error(err);
+        });
+    }
 
-async function startCamera() {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    video.srcObject = stream;
-    video.play();
-  } catch (error) {
-    console.error('Error accessing camera:', error);
-  }
-}
-
-snapButton.addEventListener('click', () => {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  context.drawImage(video, 0, 0);
-  canvas.style.display = 'block';
-});
-
-startCamera();
+    snapButton.addEventListener('click', function() {
+        console.log('snap');
+    });
+})();
